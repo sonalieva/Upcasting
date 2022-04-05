8b25fbe0829efc80ef4311a34ab184e93652b4d8
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-04-05 07:33:41 +0400

    Add files via upload

---
+++ ExtensionMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hometask0404
+{
+    public static class ExtensionMethods
+    {
+        public static void CustomContains()
+        {
+
+        }
+        public static void CustomContains(string word)
+        {
+
+        }
+        public static void CustomContains(char letter)
+        {
+
+        }
+
+        public static bool IsPrime(this int obj)
+        {
+            if (obj == 1 || obj == 0)
+            {
+            return false;
+            }
+            else if (obj==2)
+            {
+                return true;
+            }
+            else if (obj%2==0)
+            {
+                return false;
+            }
+            for (int i = 2; i*i < obj; i++)
+            {
+                if (obj%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hometask0404
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int obj = 21;          
+            Console.WriteLine(obj.IsPrime());
+        }
+    }
+}
